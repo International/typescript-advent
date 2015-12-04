6849a6f5b3198f68b31c25cef4af8ffd76fd675d
George Opritescu <ssscripting@gmail.com>
2015-12-04 21:57:05 +0100

    added grunt

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function (grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    ts: {
+      build: {
+        src: ["challenges/*.ts", "!node_modules/**/*.ts"], 
+        // Avoid compiling TypeScript files in node_modules
+        options: {
+          module: 'commonjs', 
+          // To compile TypeScript using external modules like NodeJS
+          fast: 'never' 
+          // You'll need to recompile all the files each time for NodeJS
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks("grunt-ts");
+
+  // Default tasks.
+  grunt.registerTask('default', ["ts:build"]);
+};
